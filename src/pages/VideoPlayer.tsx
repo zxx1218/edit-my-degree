e08dcc631af7d8af1639dc2915e44df9cf92c091
zxx1218 <zxx12182022@163.com>
2025-12-12 13:42:58 +0800

    演示视频页修改内容

--- src/pages/VideoPlayer.tsx
+++ src/pages/VideoPlayer.tsx
@@ -17,7 +17,7 @@ const VideoPlayer = () => {
         { number: "2", text: "单机具体卡片进入详情页可以修改个人信息，但上传的照片保存会比较慢，请等待系统弹出保存成功的提示", isWarning: false },
         { number: "3", text: "系统没有登陆时间限制，能否登陆只看登录次数余额", isWarning: false },
         { number: "4", text: "上传照片请注意照片不要太大，过大的高清照片会导致您每次点击详情页面的等待加载时间延长", isWarning: false },
-        { number: "4", text: "请勿将系统至于后台太久，时间太久会触发自动退出", isWarning: false },
+        { number: "3", text: "每生成一份报告需消耗30个PDF积分，永久版用户开通后会赠送30个积分，PDF积分充值请在闲鱼下单", isWarning: false },
         { number: "5", text: "如果您是第一次使用，建议您先使用电脑登录后将所有信息设置好，再使用手机登录查看", isWarning: true },
         { number: "6", text: "特别注意：在系统内修改的各项信息，不会同步到自己的真实学信网上", isWarning: true }
       ]
