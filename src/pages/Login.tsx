c3b6537a2f88a5f1e080a09a59432d535a9653ea
zxx1218 <zxx12182022@163.com>
2025-12-15 10:35:35 +0800

    up

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -464,7 +464,7 @@ const Login = () => {
           </Alert>
 
           <div className="mt-6 text-center text-xs text-muted-foreground/70 border-t border-border/50 pt-4">
-            <div>当前版本：V3.0.3 • 更新时间：2025.12</div>
+            <div>当前版本：V3.1.0 • 更新时间：2025.12</div>
           </div>
         </CardContent>
       </Card>
